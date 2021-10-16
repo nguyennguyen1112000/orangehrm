bf1b30f6fa0586e25e14adb7170e5b812740fc60
nguyennguyen1112000 <nguyennguyen1112000@gmail.com>
2021-10-16 16:58:11 +0700

    first commit

---
+++ symfony/web/resource_dir_inc.php
@@ -0,0 +1,5 @@
+<?php 
+sfConfig::set('sf_web_css_dir_name','webres_6051af48107ce6.31500353/css');
+sfConfig::set('sf_web_js_dir_name','webres_6051af48107ce6.31500353/js');
+sfConfig::set('sf_web_images_dir_name','webres_6051af48107ce6.31500353/images');
+sfConfig::set('ohrm_resource_dir', 'webres_6051af48107ce6.31500353');
